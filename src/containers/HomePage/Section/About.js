0f2171f8ce19ed9d3267b59bf28fa8f0b2e67596
NguyenTriBao <tribao0510@gmail.com>
2024-05-08 22:51:20 +0700

    Full done display

---
+++ src/containers/HomePage/Section/About.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+
+
+
+class About extends Component {
+
+
+    render() {
+        return (
+            <div className='section-share section-about'>
+                <div className='section-about-header'>
+                    Truyền thông nói gì
+                </div>
+                <div className='section-about-content'>
+                    <div className='content-left'>
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/jk7LbXUpmz0" title="STREAM ĐẾN BAO GIỜ - ĐỘ MIXI ft. BẠN SÁNG TÁC | OFFICIAL MUSIC VIDEO" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    </div>
+                    <div className='content-right'>
+                        <p>aafgfgffffffff</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    ƒ
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
+
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);
