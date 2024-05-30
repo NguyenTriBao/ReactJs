6d0879d6257e6b3ef59c876ce9f3860bf6b7ad1a
NguyenTriBao <tribao0510@gmail.com>
2024-05-30 09:10:50 +0700

    create-new-specialty

---
+++ src/containers/System/Specialty/ManageSpecialty.js
@@ -0,0 +1,115 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from "react-redux";
+import { LANGUAGES, CommonUtils } from '../../../utils';
+import { FormattedMessage } from 'react-intl';
+import './ManageSpecialty.scss';
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+import { createNewSpecialty } from '../../../services/userService';
+import { toast } from 'react-toastify';
+
+const mdParser = new MarkdownIt();
+class ManageSpecialty extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            imageBase64: '',
+            descriptionHTML: '',
+            descriptionMarkdown: '',
+        }
+    }
+    async componentDidMount() {
+
+    }
+    async componentDidUpdate(preProps, preState, snapshot) {
+        if (this.props.language !== preProps.language) {
+
+        }
+
+    }
+    handleOnChangeInput = (event, id) => {
+        let stateCopy = { ...this.state }
+        stateCopy[id] = event.target.value;
+        this.setState({
+            ...stateCopy
+        })
+
+    }
+    handleEditorChange = ({ html, text }) => {
+        this.setState({
+            descriptionHTML: html,
+            descriptionMarkdown: text,
+        })
+    }
+    handleOnchageImage = async (event) => {
+        let data = event.target.files;
+        let file = data[0];
+        if (file) {
+            let base64 = await CommonUtils.getBase64(file);
+            this.setState({
+                imageBase64: base64
+            })
+
+        }
+    }
+    handleSaveNewSpecialty = async () => {
+        let res = await createNewSpecialty(this.state);
+        if(res && res.errCode === 0){
+            toast.success('Save new specialty succeed!')
+        }
+        else{
+            toast.error('Save new specialty failed!')
+        }
+    }
+    render() {
+       
+        return (
+            <div className='manage-specialty-container'>
+                <div className='ms-title'>
+                    Quản lý chuyên khoa
+                </div>
+
+                <div className='add-new-specialty row'>
+                    <div className='col-6 form-group'>
+                        <label>Ten chuyen khoa</label>
+                        <input className='form-control' type='text'
+                            value={this.state.name}
+                            onChange={(event) => this.handleOnChangeInput(event, 'name')} />
+                    </div>
+                    <div className='col-6 form-group'>
+                        <label>anh chuyen khoa</label>
+                        <input className='form-control-file' type='file'
+                            onChange={(event => this.handleOnchageImage(event))} />
+                    </div>
+                    <div className='col-12'>
+                        <MdEditor style={{ height: '300px' }} renderHTML={text => mdParser.render(text)}
+                            onChange={this.handleEditorChange}
+                            value={this.state.descriptionMarkdown}
+                        />
+                    </div>
+                    <div className='col-12'>
+                        <button className='btn-save-specialty'
+                        onClick={()=>this.handleSaveNewSpecialty()}
+                        >Save</button>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageSpecialty);
